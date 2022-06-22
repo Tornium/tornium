54ec4421deec229ba3f149b6104d239d933fcdba
tiksan <webmaster.deeksh@gmail.com>
2022-06-22 08:00:27 +0000

    Bug fix

--- static/torn/factions.js
+++ static/torn/factions.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
                     membersTable.row.add([
                         member["username"],
                         member["level"],
-                        `<span data-bs-toggle="tooltip" data-bs-placement="right" title="Last Action: ${member["last_action"]}>${member["status"]}</span>`,
+                        `<span data-bs-toggle="tooltip" data-bs-placement="right" title="Last Action: ${member["last_action"]}">${member["status"]}</span>`,
                         `<a href="https://discordapp.com/users/${member["discord_id"]} style="color: black" target="_blank" rel="noopener noreferer">${member["discord_id"]}</a>`
                     ]).draw();
                 });

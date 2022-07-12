d121437806e97a18a4f829d16a9f631a3b70637e
tiksan <webmaster.deeksh@gmail.com>
2022-07-12 05:57:43 +0000

    Updated logging level

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -45,7 +45,7 @@ logger: logging.Logger = logging.getLogger("celeryerrors")
 if get_redis().get("dev"):
     logger.setLevel(logging.DEBUG)
 else:
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename="celeryerrors.log", encoding="utf-8", mode="a")
 handler.setFormatter(
     logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")

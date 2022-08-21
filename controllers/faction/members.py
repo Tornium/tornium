0721397eb3ae82194ae5c343c32e99dee8fef705
dssecret <webmaster@deek.sh>
2022-08-21 18:24:18 -0500

    Fixed #46
    Signed-off-by: dssecret <webmaster@deek.sh>

--- controllers/faction/members.py
+++ controllers/faction/members.py
@@ -7,6 +7,7 @@ import math
 
 from flask import render_template, request
 from flask_login import login_required
+from mongoengine.queryset.visitor import Q
 
 from controllers.faction.decorators import *
 from models.usermodel import UserModel
@@ -16,13 +17,21 @@ from models.usermodel import UserModel
 @fac_required
 def members(*args, **kwargs):
     fac_members = UserModel.objects(factionid=kwargs["faction"].tid)
-    stats = [member.battlescore for member in fac_members]
+    stats = []
+
+    member: UserModel
+    for member in fac_members:
+        if member.battlescore == 0:
+            continue
+
+        stats.append(member.battlescore)
+
     stats.sort(reverse=True)
     stats = stats[: math.floor(0.8 * len(stats)) - 1]
 
     return render_template(
         "faction/members.html",
         members=fac_members,
-        average_stat=fac_members.average("battlescore"),
+        average_stat=UserModel.objects(Q(factionid=kwargs["faction"].tid) & Q(battlescore__ne=0)).average("battlescore"),
         average_stat_80=sum(stats) / len(stats),
     )

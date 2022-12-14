ed0a09b80ed2bf53337eaa35a2b87587d4bc63f9
dssecret <webmaster@deek.sh>
2022-12-13 23:30:37 -0600

    ping bug fix
    Signed-off-by: dssecret <webmaster@deek.sh>

--- skynet/commands/__init__.py
+++ skynet/commands/__init__.py
@@ -7,20 +7,18 @@ from skynet.commands import faction, user, bot
 
 
 def ping(interaction):
-    return (
-        {
-            "type": 4,
-            "data": {
-                "embeds": [
-                    {
-                        "title": "Pong",
-                        "image": {
-                            "url": "https://media3.giphy.com/media/pWncxUrrNHdny/giphy.gif"
-                        },
-                        "color": 0x7DF9FF,
-                    }
-                ],
-                "flags": 64,  # Ephemeral
-            },
+    return {
+        "type": 4,
+        "data": {
+            "embeds": [
+                {
+                    "title": "Pong",
+                    "image": {
+                        "url": "https://media3.giphy.com/media/pWncxUrrNHdny/giphy.gif"
+                    },
+                    "color": 0x7DF9FF,
+                }
+            ],
+            "flags": 64,  # Ephemeral
         },
-    )
+    }

e7f2b879a75211fdb3b647b3fdf7456e610f6229
dssecret <webmaster@deek.sh>
2023-01-07 00:07:25 -0600

    stocks notify style fix
    Signed-off-by: dssecret <webmaster@deek.sh>

--- tasks/stocks.py
+++ tasks/stocks.py
@@ -118,7 +118,8 @@ def fetch_stock_ticks():
                         },
                         {
                             "name": "Target Price",
-                            "value": f"${utils.commas(notification.target)}"
+                            "value": f"${utils.commas(notification.target)}",
+                            "inline": True,
                         },
                         {
                             "name": "Current Price",

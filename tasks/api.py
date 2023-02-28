4a6ca54bc2dd5ab8bc90170d5d91d9cff9352258
dssecret <webmaster@deek.sh>
2023-02-28 14:01:13 -0600

    test fix
    Signed-off-by: dssecret <webmaster@deek.sh>

--- tasks/api.py
+++ tasks/api.py
@@ -13,10 +13,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from gevent import monkey
-
-monkey.patch_all()
-
 import datetime
 import json
 import math

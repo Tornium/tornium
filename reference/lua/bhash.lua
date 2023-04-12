dff73c20c305e7e32fd469ba452eca08802dc2ad
dssecret <webmaster@deek.sh>
2023-04-12 17:47:52 -0500

    Reimplement DBucket to primarily utilize Lua scripts
    Signed-off-by: dssecret <webmaster@deek.sh>

--- reference/lua/bhash.lua
+++ reference/lua/bhash.lua
@@ -1,7 +1,7 @@
 local bhash = redis.call("GET", KEYS[1])
 
 if bhash == false then
-    redis.call("SET", KEYS[1] .. ":lock", 1)
+    redis.call("SET", KEYS[1] .. ":lock:" .. ARGV[1], 1, "NX", "EX", 2)
     return bhash
 end
 

f504e64b9befa80620941be991bacc81d1526dff
dssecret <webmaster@deek.sh>
2023-04-21 18:59:51 -0500

    Fixed old references to FactionModel.vaultconfig
    Signed-off-by: dssecret <webmaster@deek.sh>

--- tornium_commons/models/factionmodel.py
+++ tornium_commons/models/factionmodel.py
@@ -29,8 +29,7 @@ class FactionModel(DynamicDocument):
     last_attacks = IntField(default=0)
 
     guild = IntField(default=0)  # Guild ID of the faction's guild
-    config = DictField(default={"vault": 0, "stats": 1})  # Dictionary of faction's bot configuration
-    vaultconfig = DictField(default={"banking": 0, "banker": 0})  # Dictionary of vault configuration
+    config = DictField(default={"stats": 1})  # Dictionary of faction's bot configuration
 
     statconfig = DictField(default={"global": 0})  # Dictionary of target config
 

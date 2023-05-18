25625ef313be2c684cd0f6b408fcf6bdbef8b443
dssecret <webmaster@deek.sh>
2023-05-17 22:33:24 -0500

    [UNTESTED] Bug fix
    Signed-off-by: dssecret <webmaster@deek.sh>

--- tornium_celery/tasks/user.py
+++ tornium_celery/tasks/user.py
@@ -124,7 +124,7 @@ def update_user_self(user_data, key=None):
         user.save()
 
     if user_data["faction"]["faction_id"] != 0:
-        faction = FactionModel.objects(tid=user_data["faction"]["faction_id"])
+        faction = FactionModel.objects(tid=user_data["faction"]["faction_id"]).first()
         faction.modify(upsert=True, new=True, set__name=user_data["faction"]["faction_name"])
 
         if user_data["faction"]["position"] in ("Leader", "Co-Leader"):

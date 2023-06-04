bafcdcc94b4ff0c82e7792999027c5ba6f807b2a
dssecret <webmaster@deek.sh>
2023-06-04 03:48:37 -0500

    Disabled faction average stat score for non-AA members
    Signed-off-by: dssecret <webmaster@deek.sh>

--- controllers/faction/members.py
+++ controllers/faction/members.py
@@ -16,7 +16,7 @@
 import math
 
 from flask import render_template
-from flask_login import login_required
+from flask_login import current_user, login_required
 from mongoengine.queryset.visitor import Q
 from tornium_commons.models import UserModel
 
@@ -27,23 +27,30 @@ from controllers.faction.decorators import fac_required
 @fac_required
 def members(*args, **kwargs):
     fac_members = UserModel.objects(factionid=kwargs["faction"].tid)
-    stats = []
 
-    member: UserModel
-    for member in fac_members:
-        if member.battlescore == 0:
-            continue
+    if current_user.factionaa:
+        stats = []
 
-        stats.append(member.battlescore)
+        member: UserModel
+        for member in fac_members:
+            if member.battlescore == 0:
+                continue
 
-    stats.sort(reverse=True)
-    stats = stats[: math.floor(0.8 * len(stats)) - 1]
+            stats.append(member.battlescore)
 
-    return render_template(
-        "faction/members.html",
-        members=fac_members,
-        average_stat=UserModel.objects(Q(factionid=kwargs["faction"].tid) & Q(battlescore__ne=0)).average(
-            "battlescore"
-        ),
-        average_stat_80=sum(stats) / len(stats) if len(stats) != 0 else 0,
-    )
+        stats.sort(reverse=True)
+        stats = stats[: math.floor(0.8 * len(stats)) - 1]
+
+        return render_template(
+            "faction/members.html",
+            members=fac_members,
+            average_stat=UserModel.objects(Q(factionid=kwargs["faction"].tid) & Q(battlescore__ne=0)).average(
+                "battlescore"
+            ),
+            average_stat_80=sum(stats) / len(stats) if len(stats) != 0 else 0,
+        )
+    else:
+        return render_template(
+            "faction/members.html",
+            members=fac_members,
+        )

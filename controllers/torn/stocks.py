a19cdca56cba3f93c26d7a59ab9c93cdfd6e0f59
dssecret <webmaster@deek.sh>
2023-06-30 15:00:51 -0500

    [in-dev] stocks ui perf fix
    Signed-off-by: dssecret <webmaster@deek.sh>

--- controllers/torn/stocks.py
+++ controllers/torn/stocks.py
@@ -13,11 +13,14 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import datetime
 import typing
 
+import mongoengine
 from flask import render_template
 from flask_login import current_user, login_required
 from tornium_celery.tasks.api import tornget
+from tornium_commons import rds
 from tornium_commons.models import ItemModel, TickModel
 
 
@@ -32,13 +35,23 @@ def stocks():
 
     ItemModel.update_items(tornget, current_user.key)
 
-    last_tick: typing.Optional[TickModel] = TickModel.objects().order_by("-timestamp").first()
+    last_tick: typing.Optional[TickModel] = TickModel.objects().order_by("-_id").first()
 
     if last_tick is None:
         return render_template(
             "errors/error.html", title="No Stocks Data", error="No stocks data was found in the database."
         )
 
-    ticks = TickModel.objects(timestamp=last_tick.timestamp)
+    stocks_map = rds().json().get("tornium:stocks")
+
+    if stocks_map is None:
+        ticks: typing.Union[list, mongoengine.QuerySet] = TickModel.objects(timestamp=last_tick.timestamp)
+    else:
+        stock_id_list = [int(stock_id) for stock_id in stocks_map.keys()]
+        now = int(datetime.datetime.utcnow().replace(second=0, microsecond=0, tzinfo=datetime.timezone.utc).timestamp())
+        ticks: typing.Union[list, mongoengine.QuerySet] = [
+            TickModel.objects(tick_id=int(bin(stock_id), 2) + int(bin(now << 8), 2)).first()
+            for stock_id in stock_id_list
+        ]
 
     return render_template("torn/stocks.html", stocks=ticks)

7e7e6f6a74a3750ef8af6eca22adb906d75f255b
dssecret <webmaster@deek.sh>
2023-07-22 16:00:54 -0500

    Bug fix
    Signed-off-by: dssecret <webmaster@deek.sh>

--- controllers/api/bot/config.py
+++ controllers/api/bot/config.py
@@ -40,12 +40,12 @@ def jsonified_server_config(guild: ServerModel):
             "channel": str(guild.assistschannel),
             "factions": guild.assist_factions,
             "roles": {
-                "smoker": guild.assist_smoker_roles,
-                "tear": guild.assist_tear_roles,
-                "l0": guild.assist_l0_roles,
-                "l1": guild.assist_l1_roles,
-                "l2": guild.assist_l2_roles,
-                "l3": guild.assist_l3_roles,
+                "smoker": list(map(str, guild.assist_smoker_roles)),
+                "tear": list(map(str, guild.assist_tear_roles)),
+                "l0": list(map(str, guild.assist_l0_roles)),
+                "l1": list(map(str, guild.assist_l1_roles)),
+                "l2": list(map(str, guild.assist_l2_roles)),
+                "l3": list(map(str, guild.assist_l3_roles)),
             },
         },
         "oc": {

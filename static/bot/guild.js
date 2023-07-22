c57127a1f9d4f535797aa56b247d5894cf15a144
dssecret <webmaster@deek.sh>
2023-07-22 14:34:15 -0500

    UI update
    Signed-off-by: dssecret <webmaster@deek.sh>

--- static/bot/guild.js
+++ static/bot/guild.js
@@ -13,8 +13,6 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 
-const assistMod = document.currentScript.getAttribute("data-assist-mod");
-
 $(document).ready(function () {
     let serverConfig = null;
     let xhttp = new XMLHttpRequest();

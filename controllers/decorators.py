eb7ed448e8b39bc02a64d8d97ac2e22bb7ddc077
tiksan <webmaster@deek.sh>
2024-01-10 12:45:47 -0800

    Updated admin_required decorator

--- controllers/decorators.py
+++ controllers/decorators.py
@@ -19,7 +19,7 @@ from functools import partial, wraps
 
 from flask import abort, redirect, render_template, request, url_for
 from flask_login import current_user, login_fresh
-from tornium_commons import rds
+from tornium_commons import Config, rds
 
 
 def token_required(f=None, setnx=False):
@@ -76,7 +76,7 @@ def admin_required(f):
     def wrapper(*args, **kwargs):
         if not current_user.is_authenticated or not login_fresh():
             return redirect(url_for("authroutes.login")), 401
-        elif not current_user.admin:
+        elif not current_user.tid not in Config.from_json().admin_users:
             return render_template("errors/admin_denied.html"), 403
 
         return f(*args, **kwargs)

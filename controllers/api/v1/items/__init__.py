0087b414ee92936ec13e25e22b415a642be1b754
tiksan <webmaster@deek.sh>
2024-01-17 09:30:51 -0800

    Item name map cache test fix

--- controllers/api/v1/items/__init__.py
+++ controllers/api/v1/items/__init__.py
@@ -14,33 +14,17 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from flask import jsonify
-from tornium_commons import rds
 from tornium_commons.models import Item
 
-from controllers.api.v1.decorators import (
-    authentication_required,
-    global_cache,
-    ratelimit,
-)
-from controllers.api.v1.utils import api_ratelimit_response, make_exception_response
+from controllers.api.v1.decorators import authentication_required, ratelimit
+from controllers.api.v1.utils import api_ratelimit_response
 
 
 @authentication_required
 @ratelimit
-@global_cache(duration=86400)
 def item_name_map(*args, **kwargs):
-    key = f"tornium:ratelimit:{kwargs['user'].tid}"
-    redis_client = rds()
-
-    torn_items = redis_client.hgetall("tornium:items:name-map")
-
-    if len(torn_items) == 0:
-        torn_items = {item.tid: item.name for item in Item.select(Item.tid, Item.name)}
-
-        if len(torn_items) == 0:
-            return make_exception_response("1000", key, details={"element": "torn_items"})
-
-        redis_client.hset("tornium:items:name-map", mapping=torn_items)
-        redis_client.expire("tornium:items:name-map", 86400)
-
-    return jsonify({"items": torn_items}), 200, api_ratelimit_response(key)
+    return (
+        jsonify({"items": Item.item_name_map()}),
+        200,
+        api_ratelimit_response(f"tornium:ratelimit:{kwargs['user'].tid}"),
+    )

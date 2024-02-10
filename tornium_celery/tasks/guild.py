c06b72d23376ad415a01cba872443f844ae4b49b
tiksan <webmaster@deek.sh>
2024-02-10 13:17:49 -0800

    Fix verify_guilds modulo expression
    a la [docs](https://docs.peewee-orm.com/en/latest/peewee/query_operators.html#adding-user-defined-operators), the modulo operator needs to be escaped with a percent sign due to string formatting

--- tornium_celery/tasks/guild.py
+++ tornium_celery/tasks/guild.py
@@ -161,14 +161,14 @@ def refresh_guilds():
 )
 def verify_guilds():
     def _modulo(lhs, rhs):
-        return Expression(lhs, "%", rhs)
+        return Expression(lhs, "%%", rhs)
 
     # This task will run every 15 minutes.
     # Each server will be part of a certain run of the task based on the server ID
-    for guild in Server.select().where(
+    for guild in Server.select(Server.sid).where(
         (Server.verify_enabled == True)
         & (Server.auto_verify_enabled)
-        & (_modulo(Server.sid, 96) == (int(time.time()) // 3600) % 96)
+        & (_modulo(Server.sid, 96) == (int(time.time()) // 3600 % 96))
     ):
         verify_users.delay(
             guild_id=guild.sid,

2c3822afc027a2bb46d7659ee0efe6a5cf66fb64
tiksan <webmaster@deek.sh>
2024-08-31 17:03:50 -0500

    Add license to Elixir worker
    Signed-off-by: tiksan <webmaster@deek.sh>

--- worker/lib/discord/events/guild_member_add.ex
+++ worker/lib/discord/events/guild_member_add.ex
@@ -1,9 +1,24 @@
+# Copyright (C) 2021-2023 tiksan
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 defmodule Tornium.Discord.Events.GuildMemberAdd do
   require Logger
 
-  @spec check_server_configuration(guild_id :: integer()) :: {:ok} | {:error}
-  defp check_server_configuration(guild_id) do
-    
+  @spec check_server_configuration(guild_id :: integer()) :: :ok | {:error, any()}
+  defp check_server_configuration(_guild_id) do
+    :ok
   end
 
   @spec handle(guild_id :: integer(), new_member :: Nostrum.Struct.Guild.Member.t()) :: nil
--- worker/mix.exs
+++ worker/mix.exs
@@ -1,3 +1,18 @@
+# Copyright (C) 2021-2023 tiksan
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 defmodule Tornium.MixProject do
   use Mix.Project
 
@@ -24,6 +39,7 @@ defmodule Tornium.MixProject do
       {:ecto_sql, "~> 3.10"},
       {:nostrum, "~> 0.10"},
       {:postgrex, ">= 0.0.0"},
+      {:tornex, git: "https://github.com/Tornium/tornex.git", ref: "2d5674cc61a0dc22e5b54ffed607f00a51ee8621"}
     ]
   end
 end

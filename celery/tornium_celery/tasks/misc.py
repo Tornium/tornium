aaad4834cac02759e42f111a96c4292a662eba32
tiksan <webmaster@deek.sh>
2024-09-26 15:32:28 -0500

    Remove unused tasks.misc.remove_unknown_channel
    Signed-off-by: tiksan <webmaster@deek.sh>

--- celery/tornium_celery/tasks/misc.py
+++ celery/tornium_celery/tasks/misc.py
@@ -13,9 +13,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from peewee import DoesNotExist
 from tornium_commons import rds
-from tornium_commons.models import TornKey, User
 
 import celery
 
@@ -44,56 +42,3 @@ def send_dm(discord_id: int, payload: dict):
 
     return discordpost.delay(endpoint=f"channels/{channel_id}/messages", payload=payload)
 
-
-# TODO: Rewrite this section to be more efficient
-# @celery.shared_task(name="tasks.misc.remove_unknown_channel", routing_key="quick.remove_unknown_channel", queue="quick")
-# def remove_unknown_channel(channel_id: int):
-#     channel_id = int(channel_id)
-#
-#     faction_od_channel: QuerySet = FactionModel.objects(od_channel=channel_id)
-#
-#     notifications: QuerySet = NotificationModel.objects(target=channel_id)
-#
-#     server_verify_channel: typing.Optional[ServerModel] = ServerModel.objects(verify_log_channel=channel_id).first()
-#     server_assist_channel: typing.Optional[ServerModel] = ServerModel.objects(assistschannel=channel_id).first()
-#     server_feed_channel: typing.Optional[ServerModel] = ServerModel.objects(stocks_channel=channel_id).first()
-#
-#     faction: FactionModel
-#     for faction in faction_od_channel:
-#         faction.chainod["odchannel"] = 0
-#         faction.save()
-#
-#     if notifications.count() > 0:
-#         notifications.delete()
-#
-#     if server_verify_channel is not None:
-#         server_verify_channel.verify_log_channel = 0
-#         server_verify_channel.save()
-#
-#     if server_assist_channel is not None:
-#         server_assist_channel.assistschannel = 0
-#         server_assist_channel.save()
-#
-#     if server_feed_channel is not None:
-#         server_feed_channel.stocks_channel = 0
-#         server_feed_channel.save()
-#
-#     server: ServerModel
-#     for server in ServerModel.objects():
-#         for faction_id, faction_oc in server.oc_config.copy().items():
-#             if int(faction_oc["ready"]["channel"]) == channel_id:
-#                 server.oc_config[faction_id]["ready"]["channel"] = 0
-#             if int(faction_oc["delay"]["channel"]) == channel_id:
-#                 server.oc_config[faction_id]["delay"]["channel"] = 0
-#
-#         for faction_id, faction_retal in server.retal_config.copy().items():
-#             if int(faction_retal["channel"]) == channel_id:
-#                 server.retal_config[faction_id]["channel"] = 0
-#
-#         for faction_id, faction_banking in server.banking_config.copy().items():
-#             if int(faction_banking["channel"]) == channel_id:
-#                 server.banking_config[faction_id]["channel"] = 0
-#
-#         server.save()
-#
-#

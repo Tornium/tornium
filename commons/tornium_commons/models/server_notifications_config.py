5c528067fd55e96ecb862697e61ee7764595d68f
tiksan <webmaster@deek.sh>
2024-12-17 19:02:32 -0600

    Reformat
    Signed-off-by: tiksan <webmaster@deek.sh>

--- commons/tornium_commons/models/server_notifications_config.py
+++ commons/tornium_commons/models/server_notifications_config.py
@@ -13,11 +13,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from peewee import (
-    BigIntegerField,
-    BooleanField,
-    DeferredForeignKey,
-)
+from peewee import BigIntegerField, BooleanField, DeferredForeignKey
 
 from .base_model import BaseModel
 

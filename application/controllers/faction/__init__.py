e525b5dde5dc4abef176c3ec2dff42e64bc75cb1
tiksan <webmaster@deek.sh>
2025-02-10 13:15:41 -0600

    Started OCs dashboard
    Signed-off-by: tiksan <webmaster@deek.sh>

--- application/controllers/faction/__init__.py
+++ application/controllers/faction/__init__.py
@@ -15,7 +15,7 @@
 
 from flask import Blueprint, render_template
 
-from controllers.faction import banking, bot, chain, members
+from controllers.faction import banking, bot, chain, crimes, members
 
 mod = Blueprint("factionroutes", __name__)
 
@@ -31,6 +31,9 @@ mod.add_url_rule("/faction/bot", view_func=bot.bot, methods=["GET", "POST"])
 
 # Chain Routes
 mod.add_url_rule("/faction/chain", view_func=chain.chain, methods=["GET"])
+
+# Crime Routes
+mod.add_url_rule("/faction/crimes", view_func=crimes.crimes, methods=["GET"])
 
 # Member Routes
 mod.add_url_rule("/faction/members", view_func=members.members, methods=["GET"])

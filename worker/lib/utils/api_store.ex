10feafbcbb0d27b49f850e2da26a737780b1cbf7
tiksan <webmaster@deek.sh>
2025-06-25 13:48:24 -0500

    Updated API store documentation and added expiration timer
    Signed-off-by: tiksan <webmaster@deek.sh>

--- worker/lib/utils/api_store.ex
+++ worker/lib/utils/api_store.ex
@@ -8,7 +8,7 @@ defmodule Tornium.API.Store do
   # TODO: Document this genserver
   # TODO: Write tests for this genserver
 
-  @default_ttl 60
+  @default_ttl 300
 
   use GenServer
 
@@ -21,18 +21,37 @@ defmodule Tornium.API.Store do
             response: map() | :expired | :not_ready,
             expires_at: DateTime.t()
           }
+
+    def expired?(%__MODULE__{expires_at: expires_at}) do
+      DateTime.after?(DateTime.utc_now(), expires_at)
+    end
   end
 
-  @spec start_link(opts :: term()) :: GenServer.on_start()
-  def start_link(_opts) do
-    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  @spec start_link(opts :: keyword()) :: GenServer.on_start()
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
-  # TODO: Document and type `create()`
+  @doc """
+  Create a `StoredResponse` for the API call ID.
+
+  The created `StoredResponse` for the provided API call ID that will be marked as `:not_ready`
+  until updated after receiving a response from the API. This `StoredResponse` will have the default
+  TTL.
+  """
+  @spec create(api_call_id :: String.t()) :: :ok
   def create(api_call_id) do
     create(api_call_id, @default_ttl)
   end
 
+  @doc """
+  Create a `StoredResponse` for the API call ID with a custom TTL.
+
+  The created `StoredResponse` for the provided API call ID that will be marked as `:not_ready`
+  until updated after receiving a response from the API through `insert/2`. This `StoredResponse` will expire at the 
+  provided `Datetime` or after TTL seconds from when invoked.
+  """
+  @spec create(api_call_id :: String.t(), ttl :: integer() | DateTime.t()) :: :ok
   def create(api_call_id, ttl) when is_integer(ttl) do
     expires_at = DateTime.utc_now() |> DateTime.add(ttl, :second)
     create(api_call_id, expires_at)
@@ -44,19 +63,38 @@ defmodule Tornium.API.Store do
 
   @doc """
   Insert an API response into the GenServer.
+
+  Insert an API response into the `StoredResponse` for the provided API call ID. Once the API response is 
+  inserted, the response can be retrieved with `pop/1`.
   """
   @spec insert(result :: map(), api_call_id :: String.t()) :: term()
   def insert(result, api_call_id) do
     GenServer.cast(__MODULE__, {:insert, result, api_call_id})
   end
 
-  # TODO: Document and type `pop()`
+  @doc """
+  Pop an API response from the GenServer.
+
+  Remove and return the API response corresponding to the provided API call ID if it exists and 
+  is ready. If the response is not ready, `nil` will be returned instead.
+  """
+  @spec pop(api_call_id :: String.t()) :: map() | nil
   def pop(api_call_id) do
     GenServer.call(__MODULE__, {:pop, api_call_id})
   end
 
+  @doc false
+  @spec expire() :: term()
+  def expire() do
+    send(__MODULE__, :expire)
+  end
+
   @impl true
-  def init(_opts) do
+  def init(opts \\ []) do
+    if Keyword.get(opts, :expires, true) do
+      :timer.send_interval(15_000, :expire)
+    end
+
     {:ok, %{}}
   end
 
@@ -75,12 +113,15 @@ defmodule Tornium.API.Store do
   def handle_call({:pop, api_call_id}, _from, %{} = state) do
     {value, updated_state} =
       case Map.get(state, api_call_id) do
+        %StoredResponse{response: :not_ready} ->
+          {:not_ready, state}
+
+        %StoredResponse{response: :expired} ->
+          {:expired, state}
+
         %StoredResponse{response: value} when is_map(value) ->
           {_, updated_state} = Map.pop(state, api_call_id)
           {value, updated_state}
-
-        %StoredResponse{response: value} ->
-          {value, state}
 
         _ ->
           {nil, state}
@@ -89,5 +130,15 @@ defmodule Tornium.API.Store do
     {:reply, value, updated_state}
   end
 
-  # TODO: Add timer to automatically remove expired responses
+  @impl true
+  def handle_info(:expire, %{} = state) do
+    {
+      :noreply,
+      state
+      |> Enum.reject(fn {_api_call_id, %StoredResponse{} = stored_response} ->
+        StoredResponse.expired?(stored_response)
+      end)
+      |> Map.new()
+    }
+  end
 end

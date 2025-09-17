19a4cf6a3ef13c705d3fe23c56e5047d045b9a40
tiksan <webmaster@deek.sh>
2025-09-17 11:34:15 -0500

    Added docs and minimal test coverage
    Signed-off-by: tiksan <webmaster@deek.sh>

--- worker/lib/faction/overdose.ex
+++ worker/lib/faction/overdose.ex
@@ -14,8 +14,24 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 defmodule Tornium.Faction.Overdose do
+  @moduledoc """
+  Functions to collate overdose data.
+  """
+
   alias Torngen.Client.Schema.FactionContributor
 
+  @doc """
+  Map the overdose count of faction members and their IDs.
+
+  ## Examples
+
+      iex> counts([
+      ...>   %Torngen.Client.Schema.FactionContributor{in_faction: true, id: 1, value: 2},
+      ...>   %Torngen.Client.Schema.FactionContributor{in_faction: false, id: 2, value: 3}
+      ...> ])
+      %{1 => 2}
+  """
+  @spec counts(overdose_data :: [FactionContributor.t()]) :: %{integer() => integer()}
   def counts([%FactionContributor{} | _] = overdose_data) do
     overdose_data
     |> Enum.filter(fn %FactionContributor{in_faction: in_faction} -> in_faction == true end)
@@ -23,6 +39,14 @@ defmodule Tornium.Faction.Overdose do
     |> Map.new()
   end
 
+  @doc """
+  Map the overdose data to `Tornium.Schema.OverdoseCount` without a count.
+
+  This is intended to ensure new members get inserted to the database separately from the upsert.
+  """
+  @spec map_new_counts(overdose_data :: [FactionContributor.t()], faction_id :: integer()) :: [
+          %{faction_id: integer(), user_id: integer(), count: integer(), updated_at: DateTime.t()}
+        ]
   def map_new_counts([%FactionContributor{} | _] = overdose_data, faction_id) when is_integer(faction_id) do
     overdose_data
     |> counts()
@@ -31,6 +55,12 @@ defmodule Tornium.Faction.Overdose do
     end)
   end
 
+  @doc """
+  Map the overdose data to `Tornium.Schema.OverdoseCount`.
+  """
+  @spec map_counts(overdose_data :: [FactionContributor.t()], faction_id :: integer()) :: [
+          %{faction_id: integer(), user_id: integer(), count: integer(), updated_at: DateTime.t()}
+        ]
   def map_counts([%FactionContributor{} | _] = overdose_data, faction_id) when is_integer(faction_id) do
     overdose_data
     |> counts()
@@ -39,8 +69,19 @@ defmodule Tornium.Faction.Overdose do
     end)
   end
 
+  @doc """
+  Map the updated overdose counts to `Tornium.Schema.OverdoseEvent`.
+  """
+  @spec map_events(
+          overdosed_members :: [Tornium.Schema.OverdoseCount.t()],
+          faction_id :: integer(),
+          new_member_ids :: [integer()]
+        ) :: [%{faction_id: integer(), user_id: integer(), created_at: DateTime.t(), drug: term()}]
   def map_events([%Tornium.Schema.OverdoseCount{} | _] = overdosed_members, faction_id, new_member_ids)
       when is_integer(faction_id) and is_list(new_member_ids) do
+    # TODO: Test this
+    # TODO: Determine the drug used
+
     overdosed_members
     |> Enum.reject(fn %Tornium.Schema.OverdoseCount{user_id: member_id} when is_integer(member_id) ->
       Enum.member?(new_member_ids, member_id)

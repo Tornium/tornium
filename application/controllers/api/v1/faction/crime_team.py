26a3be1d4b43965dfde0a6eca8bcca7165a75fcd
tiksan <webmaster@deek.sh>
2025-09-29 11:08:37 -0500

    Bug fix
    Signed-off-by: tiksan <webmaster@deek.sh>

--- application/controllers/api/v1/faction/crime_team.py
+++ application/controllers/api/v1/faction/crime_team.py
@@ -145,9 +145,9 @@ def delete_oc_team(faction_id: int, team_guid: str, *args, **kwargs):
     elif not kwargs["user"].can_manage_crimes():
         return make_exception_response("4006")
 
-    OrganizedCrime.where(
+    OrganizedCrime.update(assigned_team=None).where(
         (OrganizedCrime.faction_id == faction_id) & (OrganizedCrime.assigned_team_id == team_guid)
-    ).update(assigned_team=None).execute()
+    ).execute()
 
     try:
         team: OrganizedCrimeTeam = (

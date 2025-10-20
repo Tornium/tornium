2ac6ccae8729a9ae6a4f17cbaf3bc9529b088d32
tiksan <webmaster@deek.sh>
2025-10-20 17:18:09 -0500

    Added initial installation of NixOS onto a server
    Signed-off-by: tiksan <webmaster@deek.sh>

---
+++ infra/configuration.nix
@@ -0,0 +1,33 @@
+{
+  modulesPath,
+  lib,
+  pkgs,
+  ...
+} @ args:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disk-config.nix
+  ];
+  boot.loader.grub = {
+    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+    # devices = [ ];
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+  services.openssh.enable = true;
+  services.openssh.passwordAuthentication = false;
+
+  environment.systemPackages = map lib.lowPrio [
+    pkgs.curl
+    pkgs.gitMinimal
+  ];
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP56l+rwxRYqgo50JP920oiI8syFJw9k+nSfe608JjBa webmaster@deek.sh"
+  ];
+
+  system.stateVersion = "24.05";
+}
+

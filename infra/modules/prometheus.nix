4d84f670b1838904d57ee546696861974423ca03
tiksan <webmaster@deek.sh>
2025-10-23 16:34:32 -0500

    Fixed prometheus firewall and nginx scrape interval
    Signed-off-by: tiksan <webmaster@deek.sh>

--- infra/modules/prometheus.nix
+++ infra/modules/prometheus.nix
@@ -9,6 +9,12 @@
       static_configs = [
         { targets = [ "127.0.0.1:9113" ]; }
       ];
+      scrape_interval = "15s";
     }
   ];
+
+  networking.firewall.extraCommands = ''
+    iptables -A INPUT -p tcp -s 10.0.0.0/24 --dport 9090 -j ACCEPT
+    iptables -A INPUT -p tcp --dport 9090 -j DROP
+  '';
 }

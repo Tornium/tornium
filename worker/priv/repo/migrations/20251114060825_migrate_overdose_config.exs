328c303474bf7c0c0f0e86d9e85c17cc1d2d1ea8
tiksan <webmaster@deek.sh>
2025-11-14 00:25:14 -0600

    Buf fix in 41d37bf migrtion
    Signed-off-by: tiksan <webmaster@deek.sh>

--- worker/priv/repo/migrations/20251114060825_migrate_overdose_config.exs
+++ worker/priv/repo/migrations/20251114060825_migrate_overdose_config.exs
@@ -12,13 +12,20 @@ defmodule Tornium.Repo.Migrations.MigrateOverdoseConfig do
         od_channel: faction_od_channel,
         guild_id: faction_guild_id
       } when is_integer(faction_od_channel) and faction_od_channel != 0 and is_integer(faction_guild_id) and faction_guild_id != 0 -> 
-        %Tornium.Schema.ServerOverdoseConfig{
-          guid: Ecto.UUID.generate(),
-          server_id: faction_guild_id,
-          faction_id: faction_id,
-          channel: faction_od_channel
-        }
-        |> Tornium.Repo.insert(on_conflict: :nothing, conflict_target: [:server_id, :faction_id])
+        guild_exists? =
+          Tornium.Schema.Server
+          |> where([s], s.sid == ^faction_guild_id)
+          |> Tornium.Repo.exists?()
+
+        if guild_exists? do
+          %Tornium.Schema.ServerOverdoseConfig{
+            guid: Ecto.UUID.generate(),
+            server_id: faction_guild_id,
+            faction_id: faction_id,
+            channel: faction_od_channel
+          }
+          |> Tornium.Repo.insert(on_conflict: :nothing, conflict_target: [:server_id, :faction_id])
+        end
 
       _ -> nil
     end)

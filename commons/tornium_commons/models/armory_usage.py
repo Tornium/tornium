b800e05f901fdb68d6c81ee4eb297225f3a65b40
tiksan <webmaster@deek.sh>
2025-11-17 22:51:36 -0600

    [chore] reformat
    Signed-off-by: tiksan <webmaster@deek.sh>

--- commons/tornium_commons/models/armory_usage.py
+++ commons/tornium_commons/models/armory_usage.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from peewee import DateTimeField, BooleanField, ForeignKeyField, IntegerField, TextField
+from peewee import BooleanField, DateTimeField, ForeignKeyField, IntegerField, TextField
 
 from .base_model import BaseModel
 from .faction import Faction

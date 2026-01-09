67c5849287884acafa6891cd589bdb62db37fc6c
tiksan <webmaster@deek.sh>
2026-01-09 13:10:24 -0600

    [indev] Fixed pgbackrest (pending primary node support)
    Signed-off-by: tiksan <webmaster@deek.sh>

--- infra/modules/postgresql-pgbackrest.nix
+++ infra/modules/postgresql-pgbackrest.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   services.pgbackrest.enable = true;
@@ -7,26 +7,38 @@
       type = "s3";
       bundle = true;
       s3-bucket = "tornium-backups";
+      s3-uri-style = "path";
       s3-endpoint = "s3.eu-central-003.backblazeb2.com";
       s3-region = "eu-central-003";
-      s3-verifyTls = true;
+      storage-verify-tls = true;
 
       cipher-type = "aes-256-cbc";
+
+      path = "/var/lib/postgresql/16";
+    };
+  };
+  services.pgbackrest.stanzas = {
+    pg1 = {
+      instances = {
+        localhost.database = "Tornium";
+        localhost.user = "postgres";
+        localhost.path = "/var/lib/postgresql/16";
+      };
     };
   };
 
   sops.templates."pgbackrest.env" = {
     content = ''
-      PGBACKREST_REPO_CIPHER_PASS="$(cat /run/secrets/pgbackrest/aes_encryption_key)"
-      PGBACKREST_REPO_S3_KEY="$(cat /run/secrets/pgbackrest/backblaze_key)"
+      PGBACKREST_REPO1_CIPHER_PASS="${config.sops.placeholder."pgbackrest/aes_encryption_key"}"
+      PGBACKREST_REPO1_S3_KEY="${config.sops.placeholder."pgbackrest/backblaze_key_id"}"
+      PGBACKREST_REPO1_S3_KEY_SECRET="${config.sops.placeholder."pgbackrest/backblaze_key"}"
     '';
-    path = "/run/secrets/pgbackrest/.env";
     owner = "pgbackrest";
   };
 
   systemd.services."postgresql-pgbackrest-backup" = {
     serviceConfig = {
-      EnvironmentFile = "/run/secrets/pgbackrest/.env";
+      EnvironmentFile = config.sops.templates."pgbackrest.env".path;
     };
 
     description = "Backup the PostgreSQL database using pgbackrest";
@@ -36,7 +48,7 @@
     wantedBy = [ "multi-user.target" ];
     reloadTriggers = [
       "/run/secrets/postgres/password"
-      "/run/secrets/pgbackrest/aes_encryption_key"
+      config.sops.templates."pgbackrest.env".path
       "/run/secrets/pgbackrest/backblaze_key"
       "/run/secrets/pgbackrest/backblaze_key_id"
     ];
@@ -45,12 +57,13 @@
       Type = "oneshot";
       User = "postgres";
       Group = "postgres";
-      ReadOnlyPaths = [ "/run/secrets/pgbackrest/" ];
+      ReadOnlyPaths = [ config.sops.templates."pgbackrest.env".path ];
     };
 
     script = ''
       set -e
 
+      ${pkgs.pgbackrest}/bin/pgbackrest --stanza=pg1 stanza-create
       ${pkgs.pgbackrest}/bin/pgbackrest --stanza=pg1 --type=full backup
     '';
   };

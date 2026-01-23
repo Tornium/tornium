0fbc7539f3e8eff5ccb1e19326d2285550401a94
tiksan <webmaster@deek.sh>
2026-01-23 14:38:47 -0600

    Added `Tornium.EctoStacktraces` to find slow queries
    Signed-off-by: tiksan <webmaster@deek.sh>

---
+++ worker/lib/utils/ecto_stack_traces.ex
@@ -0,0 +1,31 @@
+defmodule Tornium.EctoStacktraces do
+  @moduledoc """
+  Source: https://elixirforum.com/t/tracking-down-slow-queries-in-ecto/58121/23?u=tiksan
+  """
+
+  def setup() do
+    :ets.new(:queries_and_stacktraces, [:set, :named_table, :public])
+    :telemetry.attach("ecto-stacktrace-tracking", [:tornium, :repo, :query], &handle_event/4, %{})
+  end
+
+  def filter(string) do
+    {:ok, regex} =
+      string
+      |> Regex.escape()
+      |> Regex.compile()
+
+    find = fn {query, _stacktrace, _cast_params, _measurements} = item, acc ->
+      if String.match?(query, regex) do
+        [item | acc]
+      else
+        acc
+      end
+    end
+
+    :ets.foldl(find, [], :queries_and_stacktraces)
+  end
+
+  def handle_event([:tornium, :repo, :query], measurements, metadata, _config) do
+    :ets.insert(:queries_and_stacktraces, {metadata[:query], metadata[:stacktrace], metadata[:cast_params], measurements})
+  end
+end

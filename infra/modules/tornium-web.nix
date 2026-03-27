999b7c86bca0288377c8b70840c502a8e14350b0
tiksan <webmaster@deek.sh>
2026-03-27 14:07:01 -0500

    [infra] Added missing proxy headers to `tornium-web` nginx configuration
    Signed-off-by: tiksan <webmaster@deek.sh>

--- infra/modules/tornium-web.nix
+++ infra/modules/tornium-web.nix
@@ -143,11 +143,16 @@ in {
       locations."/" = {
         proxyPass = "http://unix:/run/tornium-web/gunicorn.sock";
         recommendedProxySettings = false;
+
         extraConfig = ''
           keepalive_timeout 30;
 
           proxy_buffering off;
           proxy_request_buffering off;
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
 
           ${cloudflare.nginxRealIpRules}
         '';

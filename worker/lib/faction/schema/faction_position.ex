f1d6798f88068f60cb057411a1898b8c304173a5
tiksan <webmaster@deek.sh>
2026-04-25 14:42:58 -0500

    Updated `FactionPosition` schema and added migration
    Signed-off-by: tiksan <webmaster@deek.sh>

--- worker/lib/faction/schema/faction_position.ex
+++ worker/lib/faction/schema/faction_position.ex
@@ -14,65 +14,27 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 defmodule Tornium.Schema.FactionPosition do
+  @moduledoc """
+  Schema for each of the faction positions of a faction.
+  """
+
   use Ecto.Schema
 
   @type t :: %__MODULE__{
           pid: Ecto.UUID.t(),
           name: String.t(),
-          faction_tid: integer(),
+          faction_id: pos_integer(),
+          faction: Tornium.Schema.Faction.t(),
           default: boolean(),
-          use_medical_item: boolean(),
-          use_booster_item: boolean(),
-          use_drug_item: boolean(),
-          use_energy_refill: boolean(),
-          use_nerve_refill: boolean(),
-          loan_temporary_item: boolean(),
-          loan_weapon_armory: boolean(),
-          retrieve_loaned_armory: boolean(),
-          plan_init_oc: boolean(),
-          access_fac_api: boolean(),
-          give_item: boolean(),
-          give_money: boolean(),
-          give_points: boolean(),
-          manage_forums: boolean(),
-          manage_applications: boolean(),
-          kick_members: boolean(),
-          adjust_balances: boolean(),
-          manage_wars: boolean(),
-          manage_upgrades: boolean(),
-          send_newsletters: boolean(),
-          change_announcement: boolean(),
-          change_description: boolean()
+          permissions: [Torngen.Client.Schema.FactionPositionAbilityEnum.t()]
         }
 
   @primary_key {:pid, Ecto.UUID, autogenerate: true}
   schema "faction_position" do
     field(:name, :string)
-    field(:faction_tid, :integer)
+    belongs_to(:faction, Tornium.Schema.Faction, references: :tid)
 
     field(:default, :boolean)
-
-    field(:use_medical_item, :boolean)
-    field(:use_booster_item, :boolean)
-    field(:use_drug_item, :boolean)
-    field(:use_energy_refill, :boolean)
-    field(:use_nerve_refill, :boolean)
-    field(:loan_temporary_item, :boolean)
-    field(:loan_weapon_armory, :boolean)
-    field(:retrieve_loaned_armory, :boolean)
-    field(:plan_init_oc, :boolean)
-    field(:access_fac_api, :boolean)
-    field(:give_item, :boolean)
-    field(:give_money, :boolean)
-    field(:give_points, :boolean)
-    field(:manage_forums, :boolean)
-    field(:manage_applications, :boolean)
-    field(:kick_members, :boolean)
-    field(:adjust_balances, :boolean)
-    field(:manage_wars, :boolean)
-    field(:manage_upgrades, :boolean)
-    field(:send_newsletters, :boolean)
-    field(:change_announcement, :boolean)
-    field(:change_description, :boolean)
+    field(:permissions, {:array, :string})
   end
 end

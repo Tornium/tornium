9f9f8b288309e585020388a6ff22656c2f006def
tiksan <webmaster@deek.sh>
2026-04-28 13:44:16 -0500

    [infra] Fixed Gunicorn proxy errors
    Signed-off-by: tiksan <webmaster@deek.sh>

--- infra/modules/tornium-web.nix
+++ infra/modules/tornium-web.nix
@@ -151,6 +151,7 @@ in {
 
           proxy_buffering off;
           proxy_request_buffering off;
+          proxy_ignore_client_abort on;
           proxy_set_header Host $host;
           proxy_set_header X-Real-IP $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;

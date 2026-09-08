cddb406b31b9b9e6b97726a4e3a1541615e708e6
tiksan <webmaster@deek.sh>
2026-09-08 13:22:19 -0500

    Fixed base URl in tornium-estimate
    Signed-off-by: tiksan <webmaster@deek.sh>

--- userscripts/tornium-estimate.user.js
+++ userscripts/tornium-estimate.user.js
@@ -41,7 +41,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 (() => {
   // constants.js
   var DEBUG = false;
-  var BASE_URL = "http://127.0.0.1:5000";
+  var BASE_URL = "https://tornium.com";
   var ENABLE_LOGGING = true;
   var VERSION = "0.5.9";
   var APP_ID = "6be7696c40837f83e5cab139e02e287408c186939c10b025";
